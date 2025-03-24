adbfba62943ed1ec8a38c6764d1a1e54c0490d5b
Khairallah AL-Awady <khairallahalawadi@gmail.com>
2025-03-24 22:33:29 +0300

    chore: 🔧 Update sdk examples

--- examples/ExampleApp/Program.cs
+++ examples/ExampleApp/Program.cs
@@ -6,30 +6,13 @@ using Newtonsoft.Json.Linq;
 using PWR;
 using PWR.Models;
 
+namespace ExampleApp;
+
 class Program
 {
     static async Task Main()
     {
-        var sdk = new PwrApiSdk("https://pwrrpc.pwrlabs.io/");
-
-        ulong startingBlock = await sdk.GetLatestBlockNumber();
-        sdk.SubscribeToIvaTransactions(1234, startingBlock, (transaction) => {
-            string sender = transaction.Sender;
-            string data = transaction.Data;
-
-            // Process the hex data
-            if (data.StartsWith("0x")) data = data.Substring(2);
-            byte[] dataBytes = data.HexStringToByteArray();
-            
-            var jsonObject = JObject.Parse(Encoding.UTF8.GetString(dataBytes));
-            string action = jsonObject["action"]?.ToString();
-            
-            if (string.Equals(action, "send-message-v1", StringComparison.OrdinalIgnoreCase)) {
-                string message = jsonObject["message"]?.ToString();
-                Console.WriteLine($"Message from {sender}: {message}");
-            }
-        });
-        Console.WriteLine("Listening for transactions...");
-        Console.ReadLine();
+        // await Vidas.Run();
+        await App.Run();
     }
 }
